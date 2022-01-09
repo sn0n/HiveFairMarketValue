12bb4a65221ed212c8145d8e2595e28d15ca46a4
Rob Foraker <are.zee.dee@gmail.com>
2022-01-09 00:51:28 -0800

    minor improvements

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 let blokz = "";
 
+document.getElementById("blokz").innerHTML = "loading...";
 
 function triggered() {
     let data = JSON.parse(localStorage.getItem('status'));
@@ -12,23 +13,27 @@ function triggered() {
     document.getElementById("blokz").innerHTML += "hive dollar: $" + data.hive_dollar.usd.toFixed(2) + "<br />"
     document.getElementById("blokz").innerHTML += "<hr />";
     */
-    document.getElementById("blokz").innerHTML += "<h1>hive-engine</h1>"
+    document.getElementById("blokz").innerHTML = "<h1>hive-engine fair market value tool</h1>"
     let rate = data.litecoin.usd/data.hive.usd;
     let buyrate = rate-(rate/100);
 
-    document.getElementById("blokz").innerHTML += "<h3>SWAP.LTC</h3>Actual rate : " + rate.toFixed(4) +  "<br />"
-    document.getElementById("blokz").innerHTML += "buy less than : " + buyrate.toFixed(4) + " | (-1%)<br />"
+    document.getElementById("blokz").innerHTML += "<h3>SWAP.LTC</h3>Actual rate : " + rate.toFixed(5) +  "<br />"
     let sellrate = rate+(rate/100)
-    document.getElementById("blokz").innerHTML += "sell more than : " + sellrate.toFixed(4) + " | (+1%)"
+    document.getElementById("blokz").innerHTML += buyrate.toFixed(5) + " to " + sellrate.toFixed(5) + "<br /><small>(1% range)</small>";
+
     
     let btcrate = data.bitcoin.usd/data.hive.usd;
     let btcbuyrate = btcrate-(btcrate/100)
-    document.getElementById("blokz").innerHTML += "<h3>SWAP.BTC</h3>Actual rate : " + btcrate.toFixed(4) +  "<br />"
-    document.getElementById("blokz").innerHTML += "buy less than : " + btcbuyrate.toFixed(4) + " | (-1%)<br />"
     let btcsellrate = btcrate+(btcrate/100)
-    document.getElementById("blokz").innerHTML += "sell moar than : " + btcsellrate.toFixed(4) + " | (+1%) <br />"
+    document.getElementById("blokz").innerHTML += "<h3>SWAP.BTC</h3>Actual rate : " + btcrate.toFixed(5) +  "<br />"
+    document.getElementById("blokz").innerHTML += btcbuyrate.toFixed(5) + " to " + btcsellrate.toFixed(5) + "<br /><small>(1% range)</small>"
 
+    document.getElementById("blokz").innerHTML += "<hr /><div id='ads'>ads: </div>"
 
+    document.getElementById("blokz").innerHTML += `<div style="position: fixed; left: 50%; transform: translate(-50%, 0); text-align: center; bottom: 0px; font-size: 2em; color: white; z-index: 1;">`
+    document.getElementById("blokz").innerHTML += `  <a href="https://blokz.io" target="_blank"><img src="../images/blokz_ad.png" style="max-width: 60%;" width="728px"></a>`
+    document.getElementById("blokz").innerHTML += `</div>`
+    document.getElementById("blokz").innerHTML += `<!-- ^^^ Ad Block-->`
 
     document.getElementById("blokz").innerHTML += "<hr /><h1>internal market</h1>"
     let imrate = JSON.stringify(data['hive'].usd)/JSON.stringify(data['hive_dollar'].usd)
