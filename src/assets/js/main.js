a2a23dd3dc0190fa68df71207d97fefd52283309
Rob Foraker <are.zee.dee@gmail.com>
2022-01-09 00:28:22 -0800

    check

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,28 +3,31 @@ let blokz = "";
 
 
 function triggered() {
-    let data = JSON.parse(document.getElementById("blokz").textContent);
-    document.getElementById("blokz").innerHTML = ""
-    document.getElementById("blokz").innerHTML += "bitcoin: " + JSON.stringify(data['bitcoin'].usd) + "<br />"
-    document.getElementById("blokz").innerHTML += "litecoin: " + JSON.stringify(data['litecoin'].usd) + "<br />"
-    document.getElementById("blokz").innerHTML += "hive: " + JSON.stringify(data['hive'].usd) + "<br />"
-    document.getElementById("blokz").innerHTML += "hive dollar: " + JSON.stringify(data['hive_dollar'].usd) + "<br />"
+    let data = JSON.parse(localStorage.getItem('status'));
+    /*
+    document.getElementById("blokz").innerHTML = "Current Rates : <br />"
+    document.getElementById("blokz").innerHTML += "bitcoin: $" + data.bitcoin.usd.toFixed(2) + "<br />"
+    document.getElementById("blokz").innerHTML += "litecoin: $" + data.litecoin.usd.toFixed(2) + "<br />"
+    document.getElementById("blokz").innerHTML += "hive: $" + data.hive.usd.toFixed(2) + "<br />"
+    document.getElementById("blokz").innerHTML += "hive dollar: $" + data.hive_dollar.usd.toFixed(2) + "<br />"
     document.getElementById("blokz").innerHTML += "<hr />";
-
+    */
     document.getElementById("blokz").innerHTML += "<h1>hive-engine</h1>"
-    let rate = JSON.stringify(data['litecoin'].usd)/JSON.stringify(data['hive'].usd)
+    let rate = data.litecoin.usd/data.hive.usd;
     let buyrate = rate-(rate/100);
+
     document.getElementById("blokz").innerHTML += "<h3>SWAP.LTC</h3>Actual rate : " + rate.toFixed(4) +  "<br />"
     document.getElementById("blokz").innerHTML += "buy less than : " + buyrate.toFixed(4) + " | (-1%)<br />"
     let sellrate = rate+(rate/100)
     document.getElementById("blokz").innerHTML += "sell more than : " + sellrate.toFixed(4) + " | (+1%)"
     
-    let btcrate = JSON.stringify(data['bitcoin'].usd)/JSON.stringify(data['hive'].usd)
+    let btcrate = data.bitcoin.usd/data.hive.usd;
     let btcbuyrate = btcrate-(btcrate/100)
     document.getElementById("blokz").innerHTML += "<h3>SWAP.BTC</h3>Actual rate : " + btcrate.toFixed(4) +  "<br />"
-    document.getElementById("blokz").innerHTML += "buy less than : " + btcbuyrate.toFixed(4) + "| (-1%)<br />"
+    document.getElementById("blokz").innerHTML += "buy less than : " + btcbuyrate.toFixed(4) + " | (-1%)<br />"
     let btcsellrate = btcrate+(btcrate/100)
-    document.getElementById("blokz").innerHTML += "sell moar than : " + btcsellrate.toFixed(4) + "| (+1%) <br />"
+    document.getElementById("blokz").innerHTML += "sell moar than : " + btcsellrate.toFixed(4) + " | (+1%) <br />"
+
 
 
     document.getElementById("blokz").innerHTML += "<hr /><h1>internal market</h1>"
@@ -43,7 +46,7 @@ fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Chi
     Accept: "application/json"
   }
 }).then(response => response.json())
-  .then(data => document.getElementById("blokz").innerHTML += JSON.stringify(data))
-  .then(setTimeout(() => triggered(), 400))
+  .then(data => localStorage.setItem('status', JSON.stringify(data) ))
+  .then(setTimeout(() => triggered(), 800))
 
 
